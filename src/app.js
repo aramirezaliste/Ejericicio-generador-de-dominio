97bea90f709a14211565ea1bbc1ec02ed59f8c3f
aramirezaliste <aramirezaliste@gmail.com>
2022-10-22 15:07:31 +0000

    Avance ejercicio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronombres = ["el", "nuestro"];
+  let adjetivos = ["aburrido", "alegre", "debil"];
+  let nombre = ["animal", "marciano"];
+
+  for (let i = 0; i < pronombres.length; i++) {
+    for (let q = 0; q < adjetivos.length; q++) {
+      for (let w = 0; w < nombre.length; w++) {
+        console.log(pronombres[i] + adjetivos[q] + nombre[w] + ".com");
+      }
+    }
+  }
 };
